22de11b66532f7751f91e4c74827934795b9dbca
Linux User <black@docker.my.domain>
2019-01-28 02:56:17 +0700

    add exit code; add timeout option

--- send.sh
+++ send.sh
@@ -6,15 +6,9 @@ sms_line=$1
 sms_phone=$2
 sms_text=$3
 
-goip_login=$goip_login
-goip_password=$goip_password
-goip_scheme=$goip_scheme
-goip_host=$goip_host
-goip_port=$goip_port
 goip_url="/default/en_US/sms_info.html?type=sms"
 
 goip_request_url=$goip_scheme"://"$goip_login":"$goip_password"@"$goip_host":"$goip_port$goip_url
-
 
 post_phone=$sms_phone
 post_text=$sms_text
@@ -24,6 +18,12 @@ post_data=$post_line"&action=SMS&telnum="$post_phone"&smscontent="$post_text"&se
 
 header_content_type="Content-Type: application/x-www-form-urlencoded"
 
-curl --silent -XPOST -H "$header_content_type" -d "$post_data" "$goip_request_url" > /dev/null
+request_code=$(curl --connect-timeout "$goip_timeout" -s -o /dev/null -w "%{http_code}" -XPOST -H "$header_content_type" -d "$post_data" "$goip_request_url")
 
-echo "Message sent"
+if [ ! $request_code = 200 ];then
+  echo $request_code" Error sending message"
+  exit 1
+else
+  echo "Message sent"
+  exit 0
+fi
